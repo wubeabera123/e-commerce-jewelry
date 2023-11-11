4b094d8b58152077bd31697cd2f6e4399a5e2c4f
wubu <wubeabera123@gmail.com>
2023-11-11 22:54:38 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+           <Navigation/>
       </header>
     </div>
   );
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import Button from 'react-bootstrap/Button';
+//import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import '@mui/material/styles';
+const Navigation = () => {
+    
+    return ( 
+        <>
+          <Button variant="outlined">Get Started</Button>
+        </>  
+     );
+
+}
+ 
+export default Navigation;
